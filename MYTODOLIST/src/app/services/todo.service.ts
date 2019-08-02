9c1d24ee445ead4cc06211dd77ba1b8856b1be71
LukeSungukJung <xrjseka615@gmail.com>
2019-08-02 18:19:31 +0900

    complete

--- MYTODOLIST/src/app/services/todo.service.ts
+++ MYTODOLIST/src/app/services/todo.service.ts
@@ -4,57 +4,40 @@ import {HttpClient,HttpHeaders} from "@angular/common/http";
 
 import {Todo} from '../models/Todo';
 
-const httpOptions={
-  headers : new HttpHeaders({
+const httpOptions = {
+  headers: new HttpHeaders({
     'Content-Type': 'application/json'
   })
-};
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
-  todosUrl:string =  'https://jsonplaceholder.typicode.com/todos?_limit=5';
-  urlo: string = 'https://jsonplaceholder.typicode.com/todos';
+  todosUrl:string = 'https://jsonplaceholder.typicode.com/todos';
+  todosLimit = '?_limit=5';
 
-  constructor(private http:HttpClient) {  }
+  constructor(private http:HttpClient) { }
 
-
-  getTodos(): Observable<Todo>{
-    return this.http.get<Todo[]>(this.todosUrl);
+  // Get Todos
+  getTodos():Observable<Todo[]> {
+    return this.http.get<Todo[]>(`${this.todosUrl}${this.todosLimit}`);
   }
 
-
-  toggleCompleted(todo:Todo): Observable<any>{
-    const url = `${this.todosUrl}/${todo.id}`;
-    return this.http.put(url, todo, httpOptions);
-  }
-  deleteTodo(todo:Todo): Observable<Todo>{
+  // Delete Todo
+  deleteTodo(todo:Todo):Observable<Todo> {
     const url = `${this.todosUrl}/${todo.id}`;
     return this.http.delete<Todo>(url, httpOptions);
   }
 
-  addTodo(todo:Todo): Observable<Todo>{
+  // Add Todo
+  addTodo(todo:Todo):Observable<Todo> {
+    return this.http.post<Todo>(this.todosUrl, todo, httpOptions);
+  }
 
-    return this.http.post<Todo>(this.urlo, todo, httpOptions);
+  // Toggle Completed
+  toggleCompleted(todo: Todo):Observable<any> {
+    const url = `${this.todosUrl}/${todo.id}`;
+    return this.http.put(url, todo, httpOptions);
   }
 }
-
-/*
-return [
-  {
-    id: 1,
-    title: 'Todo1',
-    completed: false
-  },
-  {
-    id: 2,
-    title: 'Todo2',
-    completed: false
-  },
-  {
-    id:3,
-    title: 'Todo3',
-    completed: false
-  }
-];
- */
