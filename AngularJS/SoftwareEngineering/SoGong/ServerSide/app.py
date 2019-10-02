a60059cd445fc101d6c55199f0b214ecd55ad61a
LukeSungukJung <xrjseka615@gmail.com>
2019-10-03 00:23:21 +0900

    connect db-flask server

--- AngularJS/SoftwareEngineering/SoGong/ServerSide/app.py
+++ AngularJS/SoftwareEngineering/SoGong/ServerSide/app.py
@@ -3,10 +3,6 @@ from flask_cors import CORS, cross_origin
 from flask_restful import Resource, Api
 from json import dumps
 from flask_jsonpify import jsonify
-
-
-
-from Message import testModule1
 from flask_sqlalchemy import SQLAlchemy
 import os
 
@@ -14,7 +10,7 @@ import os
 app = Flask(__name__)
 api = Api(app)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
-app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///foo.db'
+app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///sqlite.db'
 CORS(app)
 
 db = SQLAlchemy(app)
@@ -29,12 +25,3 @@ def index(titlename, authorname):
     db.session.add(url)
     db.session.commit()
     return'<h1>ADdd new url!</h1>'
-    
-class test(Resource):
-    def get(self):
-        return testModule1.hello()
-api.add_resource(test, '/app-notice') # Route_1
-
-
-if __name__ == '__main__':
-     app.run(debug=True,port=5002)
