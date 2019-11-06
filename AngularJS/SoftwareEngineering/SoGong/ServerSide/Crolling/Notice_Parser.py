2b2861e0a5d70613c1f65f9417d95f644cb06982
yewonez <yewonez@naver.com>
2019-11-06 13:51:48 +0900

    이전의 버전을 사용했었네요 수정했습니다.

--- AngularJS/SoftwareEngineering/SoGong/ServerSide/Crolling/Notice_Parser.py
+++ AngularJS/SoftwareEngineering/SoGong/ServerSide/Crolling/Notice_Parser.py
@@ -2,16 +2,8 @@ import requests
 from bs4 import BeautifulSoup as bs
 
 
-#Login user info
-def make_login_info(member_id, memeber_pw):
-    LOGIN_INFO = {
-        'member_id': member_id,
-        'member_pw': memeber_pw
-    }
-    return LOGIN_INFO
-
 def get_title_from_junk(dirty_title):
-    
+
     slice_index =  min(dirty_title.index('\n'), dirty_title.index('\t'))
     refiend_title = dirty_title[:slice_index]
     return refiend_title
@@ -24,7 +16,7 @@ def parse_data_function():
         data = []
         table = soup.find('table', attrs={'class':'board-table'})
         table_body = table.find('tbody')
-        
+
         rows = table_body.find_all('tr')
         for row in rows:
             cols = row.find_all('td')
@@ -40,7 +32,7 @@ def get_a_tag():
         data = []
         table = soup.find('table', attrs={'class':'board-table'})
         table_body = table.find('tbody')
-        
+
         rows = table_body.find_all('a')
         for uf_a_tag in rows:
             start_index = str(uf_a_tag).index("\"")+1
@@ -50,15 +42,17 @@ def get_a_tag():
             #print(rf_atag)
             data.append(base_url+rf_atag)
     return data
-    
-    
+
+
 
 def make_entire_refined_data():
     res = []
-    temp_dic = {}                   
+    temp_dic = {}
     unrefined_notice_data = parse_data_function()
     rf_all_atags = get_a_tag()
     for idx,one in enumerate(unrefined_notice_data):
+        if one[0] == '공지':
+            continue
         temp_dic['title'] = get_title_from_junk(one[1])
         temp_dic['author'] = one[2]
         temp_dic['post_date'] = one[3]
@@ -67,7 +61,6 @@ def make_entire_refined_data():
         temp_dic = {}
     return res
 
-#temp = make_login_info('$$$$$$$$$', '############')                   
+#temp = make_login_info('$$$$$$$$$', '############')
 #unrefined_notice_data = login_function(temp)
-#res_crwaling_data = make_entire_refined_data(unrefined_notice_data)  
-                                             
+#res_crwaling_data = make_entire_refined_data(unrefined_notice_data)
